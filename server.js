39689ee5fd69f6eb0d3c5a9b265f6b382e535295
Mide-ly <owolabidemilade591@gmail.com>
2026-08-10 14:51:45 +0000

    j

--- server.js
+++ server.js
@@ -16,11 +16,11 @@ const transporter = nodemailer.createTransport({
   auth: { user: process.env.EMAIL_USER, pass: process.env.EMAIL_APP_PASSWORD }
 });
 
-// ==========================================
+// =========================================
 // 2. PUT YOUR WHATSAPP NUMBER HERE
 // Format: Country code + number (NO plus sign)
 // Example: "2348012345678"
-// ==========================================
+// =========================================
 const BOT_NUMBER = "2348022833007"; 
 
 let whatsappSocket;
